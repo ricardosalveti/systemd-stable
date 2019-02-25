401d7d9de773a3661384b2edaae04e7036650f0b
Chen Qi <Qi.Chen@windriver.com>
2019-02-25 14:56:21 +0800

    don't fail if GLOB_BRACE and GLOB_ALTDIRFUNC is not defined
    If the standard library doesn't provide brace
    expansion users just won't get it.
    
    Dont use GNU GLOB extentions on non-glibc systems
    
    Conditionalize use of GLOB_ALTDIRFUNC
    
    Upstream-Status: Inappropriate [musl specific]
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>
    Signed-off-by: Chen Qi <Qi.Chen@windriver.com>
    [rebased for systemd 243]
    Signed-off-by: Scott Murray <scott.murray@konsulko.com>

--- src/tmpfiles/tmpfiles.c
+++ src/tmpfiles/tmpfiles.c
@@ -63,6 +63,12 @@
 #include "umask-util.h"
 #include "user-util.h"
 
+/* Don't fail if the standard library
+ * doesn't provide brace expansion */
+#ifndef GLOB_BRACE
+#define GLOB_BRACE 0
+#endif
+
 /* This reads all files listed in /etc/tmpfiles.d/?*.conf and creates
  * them in the file system. This is intended to be used to create
  * properly owned directories beneath /tmp, /var/tmp, /run, which are
@@ -1936,7 +1942,9 @@ finish:
 
 static int glob_item(Item *i, action_t action) {
         _cleanup_globfree_ glob_t g = {
+#ifdef GLOB_ALTDIRFUNC
                 .gl_opendir = (void *(*)(const char *)) opendir_nomod,
+#endif
         };
         int r = 0, k;
         char **fn;
@@ -1956,7 +1964,9 @@ static int glob_item(Item *i, action_t action) {
 
 static int glob_item_recursively(Item *i, fdaction_t action) {
         _cleanup_globfree_ glob_t g = {
+#ifdef GLOB_ALTDIRFUNC
                 .gl_opendir = (void *(*)(const char *)) opendir_nomod,
+#endif
         };
         int r = 0, k;
         char **fn;
